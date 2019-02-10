c5c4da10eb1046ec98f1bf6018338f5528d19c40
eric3u <eric3u@outlook.com>
2019-02-10 15:33:43 +0800

    [Feat] 初始提交

---
+++ MPGravityControlLogic/MPGravityControlLogic/MPGravityControlLogic.h
@@ -0,0 +1,19 @@
+//
+//  MPGravityControlLogic.h
+//  MPGravityControlLogic
+//
+//  Created by CmST0us on 2019/2/10.
+//  Copyright © 2019 eric3u. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MPGravityControlLogic.
+FOUNDATION_EXPORT double MPGravityControlLogicVersionNumber;
+
+//! Project version string for MPGravityControlLogic.
+FOUNDATION_EXPORT const unsigned char MPGravityControlLogicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPGravityControlLogic/PublicHeader.h>
+
+
